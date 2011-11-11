5f254cf64b9e3a394539147a8f74ad620a2a53cc
Moustafa Eid <brain@brain-linux.(none)>
2011-11-10 22:57:42 -0500

    implemented views

--- src/com/steganography/steganography.java
+++ src/com/steganography/steganography.java
@@ -2,7 +2,7 @@ package com.steganography;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.widget.TextView;
 public class steganography extends Activity {
     /** Called when the activity is first created. */
     @Override
